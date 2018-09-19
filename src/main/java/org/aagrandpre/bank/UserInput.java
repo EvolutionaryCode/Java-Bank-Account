b0b3ba4c59405b6f19773055a731d83bfa20f109
Unknown <ausgran@icloud.com>
2018-09-18 20:54:31 -0400

    Updated Input Information & Such!

--- src/main/java/org/aagrandpre/bank/UserInput.java
+++ src/main/java/org/aagrandpre/bank/UserInput.java
@@ -30,53 +30,7 @@ import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
  * Project Name - Bank Project
  * Class Larson - AP Computer Science
  * 
- * Changelog - 
- * August 31st, 2018 - Added Multi-Scanner Functionality & Login System
- * September 5th 2018 - Added RethinkDB Login System Validation
- * September 6th 2018 - Added Success/Failed Login Logs To RethinkDB
- * September 7th - 9th 2018 - Setup Google Authenticator And Logged Key To RethinkDB
- * September 10th 2018 - Created Cash Suffixes
- * September 11th 2018 - Setup Register System with embeds
- * September 12th 2018 - Setup Individual Bank Accounts
- * September 12th 2018 - Rebuilt Login Around RethinkDB & Google Authenticator
- * September 13th 2018 - Built Withdraw With Google Authenticator for withdraws over 3,000
- * September 13th 2018 - Built deposit function along with logging for deposit and Withdraw
- * September 14th-Onward 2018 - All changes are now stored in GitHub Commits
- * 
- * To-Do List
- * Transfer Funds From One User To Another
- * Admin Panel Change Funds, Delete Users, Lock User Accounts
- * Automatic after 3 failed logins lockout the user for 3 minutes
- * Re-add JSON Embeds
- * Enable Multi Language Support (Already have code written to grab a RethinkDB file just need translations and switch prints)
- * Friendly user UI
- * Encrypt Passwords
- * Re-add Cash Suffixes (K, M, B, T)
- * Re-add numbers function
- * Spit across multiple classes in java
- * Re-add Message If User Doesn't Do Login, Numbers Or Auth
- * 
- * New User Guide:
- * When promoted type Register to create a new account then go through and complete the fields!
- * If you would like to enable 2FA I have used Google Authenticator - Restart the program and then type Auth and then enter the username and password
- * then, when it prompts you with the key store it in either the Google Authenticator App or Google Authenticator Chrome Plugin
- * To Access the bank restart the program and type login and then type username and password and then pick an option!
- * 
- * Project Builder Information
- * IDE - Netbeans V.8.2
- * Required Plugin's - Gradle Support
- * After installing Gradle support pick load project from zip open up the zipped file!
- * 
- * Credits -
- * Database - RethinkDB (Open Source NoSQL Database)
- * Auth - Google Authenticator (Created By Google)
- * Auth - Google Java Authenticator Connect (Created by Warrenstrange)
- * 
- * Contributors-
- * Stek - Formated Code On Stack Overflow!
- * Stack Overflaw - Helped Point Me In The Direction For RethinkDB Data Types To Java On Stack Overflow!
- * Ronz#2224 - Helped With RethinkDB Hashmaps (Data Store Embeds) & some RethinkDB to Java Syntax Questions! (Discord)
- * If you request to Push a change to improve the project feel free and all changes you push will be noted here!
+ *Github - https://github.com/EvolutionaryCode/Java-Bank-Account
  */
 
 
@@ -426,7 +380,7 @@ public class UserInput {
                            
                }else {
                     //What to do if they don't pick a valid Gkey
-                    System.out.println("Invalid Gkey!");
+                    System.out.println("Invalid Google Code!");
                 }
                           }
                     }else {
