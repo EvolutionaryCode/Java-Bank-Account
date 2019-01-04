bb14474ebeebf3443780a70e35330cc24ad53afe
Austin Grandpre <ausgran@icloud.com>
2019-01-04 09:50:20 -0500

    Updated JSON Simple to Map/Grab Nested Values
    This update allows for nested values and makes life simpler!

--- src/main/java/org/aagrandpre/bank/core/utils/SettingsGSON.java
+++ src/main/java/org/aagrandpre/bank/core/utils/SettingsGSON.java
@@ -1,30 +1,57 @@
 package org.aagrandpre.bank.core.utils;
 
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.*;
 
+public class SettingsGSON
+{
+    public static void main(String[] args) throws Exception
+    {
+        // parsing file "JSONExample.json"
+        Object obj = new JSONParser().parse(new FileReader("bankconfig.json"));
 
-public class SettingsGSON {
+        // typecasting obj to JSONObject
+        JSONObject jo = (JSONObject) obj;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        JSONParser parser = new JSONParser();
-        try {
-            // parsing file "JSONExample.json"
-            Object obj = new JSONParser().parse(new FileReader("bankconfig.json"));
-            // typecasting obj to JSONObject
-            JSONObject jo = (JSONObject) obj;
-            // getting firstName and lastName
-            String dbtype = (String) jo.get("dbconnected");
-            String address = (String) jo.get("lastName");
-            //Print out first/lastname
-            System.out.println(dbtype);
-            System.out.println(address);
-        } catch (Exception e) {
-            e.printStackTrace();
+        // getting firstName and lastName
+        String firstName = (String) jo.get("firstName");
+        String lastName = (String) jo.get("lastName");
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        // getting age
+        long age = (long) jo.get("age");
+        System.out.println(age);
+
+        // getting address
+        Map address = ((Map)jo.get("address"));
+
+        // iterating address Map
+        Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+        while (itr1.hasNext()) {
+            Map.Entry pair = itr1.next();
+            System.out.println(pair.getKey() + " : " + pair.getValue());
+        }
+
+        // getting phoneNumbers
+        JSONArray ja = (JSONArray) jo.get("phoneNumbers");
+
+        // iterating phoneNumbers
+        Iterator itr2 = ja.iterator();
+
+        while (itr2.hasNext())
+        {
+            itr1 = ((Map) itr2.next()).entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
         }
     }
 }
